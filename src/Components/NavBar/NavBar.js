075e691769acb2db81d961a27f8e8a02a195cf6a
mehedih23 <nmehedi23@gmail.com>
2022-04-16 22:44:12 +0600

    navbar added

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import './NavBar.css'
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import CustomLink from '../CustomLink/CustomLink'
+
+const NavBar = () => {
+    return (
+        <Navbar className='nav' collapseOnSelect expand="lg" >
+            <Container>
+                <CustomLink style={{ textDecoration: 'none' }} to="/">
+                    <h1 className='logo-title'> Muscle <span style={{ color: '#E8BD0D' }}>Maniacs</span></h1>
+                </CustomLink>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <CustomLink className='link' to="/services">Services</CustomLink>
+                        <CustomLink className='link' to="/aboutus">About us</CustomLink>
+                        <CustomLink className='link' to="/blog">Blog</CustomLink>
+                        <CustomLink className='link' to="/contactus">Contact Us</CustomLink>
+                    </Nav>
+                    <Nav>
+                        <CustomLink className='link' to="/signup">Sign Up</CustomLink>
+                        <CustomLink className='link' to="/login">Login</CustomLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
