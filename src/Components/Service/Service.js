17ebe7dee46dcc29206d2f6d6c95a1432fe1d183
mehedih23 <nmehedi23@gmail.com>
2022-04-17 11:08:55 +0600

    service added completely

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,26 +1,28 @@
 import './Service.css'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Service = (props) => {
     const { service } = props;
     const { image, name, price, about } = service;
 
     return (
-        <div className='service-container my-3 text-center col-lg-4 col-md-6 col-12'>
-            <div className='s-img'>
+        <div className='service-container my-3 col-lg-4 col-md-6 col-12'>
+            <div>
                 <img className='img-fluid' src={image} alt="service" />
             </div>
             <div>
                 <p>{about}</p>
             </div>
-            <div className='text-center'>
+            <div>
                 <h3>{name}</h3>
                 <h2>{price}</h2>
-
-
             </div>
             <div className='service-btn text-center'>
-                <button className=''>Add</button>
+                <button className=''>
+                    Add <FontAwesomeIcon className='ms-1' icon={faCartShopping} />
+                </button>
             </div>
         </div>
     )
